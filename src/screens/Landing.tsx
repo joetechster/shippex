73bafdb9800ff3edb43197f279528bcf39e89eb9
Joseph <joetechster@gmail.com>
2025-08-06 19:43:07 +0100

    fix splash jump

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -3,26 +3,34 @@ import Animated, { FadeIn } from 'react-native-reanimated';
 import useAnimatedHideSplashScreen from '../hooks/useAnimateHideSplashScreen';
 import LogoNamed from '../../assets/logo-named.svg';
 import LoginButton from '../components/LoginButton';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { colors } from '../constants/colors';
 import { RootStackParamList } from '../navigation/NativeStack';
 
-const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView);
 const AnimatedLogoNamed = Animated.createAnimatedComponent(LogoNamed);
 type Landing = NativeStackScreenProps<RootStackParamList, 'Landing'>;
 
 export default function Landing(props: Landing) {
   const { containerStyle, Logo } = useAnimatedHideSplashScreen();
+  const insets = useSafeAreaInsets();
 
   const onLoginButtonPress = () => {
     props.navigation.navigate('Login');
   };
 
   return (
-    <AnimatedSafeAreaView style={[styles.container, containerStyle]}>
+    <Animated.View style={[styles.container, containerStyle]}>
       {Logo || (
-        <View style={styles.contentContainer}>
+        <View
+          style={[
+            styles.contentContainer,
+            { paddingTop: insets.top, paddingBottom: insets.bottom },
+          ]}
+        >
           <AnimatedLogoNamed
             entering={FadeIn}
             style={styles.logoNamed}
@@ -35,7 +43,7 @@ export default function Landing(props: Landing) {
         </View>
       )}
       <StatusBar barStyle="light-content" backgroundColor={colors.primary} />
-    </AnimatedSafeAreaView>
+    </Animated.View>
   );
 }
 
